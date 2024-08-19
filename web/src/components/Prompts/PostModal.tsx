60dfb4e29e51cb139e8c67f3bb6df7548c6d7b98
Uthsob Chakraborty <uthsob9@gmail.com>
2024-08-19 15:13:00 +0600

    Beautify create  post modal and added topic field

--- web/src/components/Prompts/PostModal.tsx
+++ web/src/components/Prompts/PostModal.tsx
@@ -9,6 +9,7 @@ import { handlePostModal } from 'store/prompt';
 export default function PostModal() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [hashtags, setHashtags] = useState('');
   const { postModal } = useSelector((state: RootState) => state.prompt);
   const dispatch = useDispatch();
   const [createPost, { isLoading }] = useCreatePostMutation();
@@ -26,6 +27,7 @@ export default function PostModal() {
   const resetStates = () => {
     setTitle('');
     setContent('');
+    setHashtags('');
   };
 
   const handleClose = () => {
@@ -42,11 +44,19 @@ export default function PostModal() {
       {...postModal}
     >
       <div className="flex flex-col gap-5 pt-5">
+        <h3> Title: </h3>
         <Input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <h4> Your Voice: </h4>
         <TextArea
           value={content}
           onChange={(e) => setContent(e.target.value)}
           rows={8}
+        />
+        <h4> Topic: </h4>
+        <Input
+          value={hashtags}
+          onChange={(e) => setHashtags(e.target.value)}
+          placeholder="Add topics/hashtags separated by commas"
         />
         <PostType />
         <Button onClick={handlePost} loading={isLoading}>
@@ -60,7 +70,7 @@ export default function PostModal() {
 const PostType = () => {
   return (
     <div className="flex items-center justify-between border rounded-lg px-3 py-2 shadow-sm">
-      <h6 className="font-medium text-md">Add to your post</h6>
+      <h6 className="font-medium text-md">Add media to your post</h6>
       <div className="flex items-center gap-1.5">
         <div className="cursor-pointer hover:bg-gray-100 rounded-full p-1">
           <FcGallery className="text-2xl" />
